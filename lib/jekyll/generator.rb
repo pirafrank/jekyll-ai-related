f335b2f949231263be8298adf9d651253a1ea3e3
Francesco Pira <dev@fpira.com>
2025-02-12 17:11:51 +0100

    first commit

---
+++ lib/jekyll/generator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "embeddings/version"
+
+module Jekyll
+  module Embeddings
+    module Generator
+      class Error < StandardError; end
+    end
+  end
+end
